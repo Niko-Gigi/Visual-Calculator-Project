38cee829fe6210ae823ecdd888f77f03d746c0cb
hugovives <hugovivesandres@gmail.com>
2024-06-11 16:30:55 +0100

    deleted epic and renamed main to test

---
+++ test.py
@@ -0,0 +1,17 @@
+#bajajo bajajo kurwa
+import matplotlib as mpl
+
+
+
+print('bajajajo bajajajo')
+print('peepeepoopoo')
+
+def func(x):
+     for i in range(x):
+        print (i)   
+     
+
+func(3) #hhhhhhh
+
+
+
