3dc35e2b7a9de95dfa71090efee37018b48fa9a8
hugovives <hugovivesandres@gmail.com>
2024-06-11 18:34:19 +0100

    added drag force parameter

--- basic_vector_field.py
+++ basic_vector_field.py
@@ -15,26 +15,29 @@ ax.set_ylim(-10, 10)
 
 # Define the vector field lol
 def vector_field(X, Y):
-    U = -Y - X
-    V = X - Y
+    U = (-Y - X)
+    V = (X - Y)
     return U, V # U and V are the x and y components of the vector arrows
 
-# Create an initial quiver plot
+# Create an initial quiver (vector) plot
 U, V = vector_field(X, Y)
 quiver = ax.quiver(X, Y, U, V) # creates an arrow with x component U and y component V with its tail at position X, Y
 
 # Set timestep
 dt = 0.1
 
+# Set drag force parameter
+drag = 0
+
 # Create an initial test ball
-X_0, Y_0, Vx_0, Vy_0 = 0.1, 3, 0, 0
+X_0, Y_0, Vx_0, Vy_0 = 0, 3, 0, 0
 ball, = ax.plot(X_0, Y_0, 'ro')
 
 # Define function to update the ball position based on acceleration and velocity
 def update_ball(frame):
     global X_0, Y_0, Vx_0, Vy_0
 
-    accx, accy = vector_field(X_0, Y_0)
+    accx, accy = vector_field(X_0, Y_0)[0]-drag*Vx_0, vector_field(X_0, Y_0)[1]-drag*Vy_0
 
     Vx_0 += accx*dt
     Vy_0 += accy*dt
@@ -49,17 +52,5 @@ def update_ball(frame):
     return ball,
 
 anim = FuncAnimation(fig, update_ball, frames=100, interval=100)
-
-'''
-# Function to update the quiver plot
-def update_quiver(num, quiver, X, Y):
-    # Update the vector field to animate
-    U, V = vector_field(X * np.cos(num / 10) - Y * np.sin(num / 10), X * np.sin(num / 10) + Y * np.cos(num / 10))
-    quiver.set_UVC(U, V)
-    return quiver,
-
-# Create an animation
-anim = FuncAnimation(fig, update_quiver, fargs=(quiver, X, Y), frames=100, interval=100)
-'''
 
 plt.show()
